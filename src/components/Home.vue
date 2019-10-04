cff9ec035874191d55dd5cb9ec0f9aae451de5b8
Tate Peñaranda <tate@tpenaranda.com>
2019-10-04 01:49:47 -0300

    Working version of Home component.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,14 +1,52 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
+    <h1>Wookie Movies</h1>
+    <b-container class="movie-row" fluid>
+      <b-row class="justify-content-md-center">
+        <b-col md="auto" v-for="movie in $root.movies" v-bind:key="movie.id" class="movie-column">
+            <b-card
+                :title="movie.title"
+                :img-src="movie.poster"
+                :img-alt="movie.slug"
+                img-top
+                tag="article"
+                style="min-width: 220px; max-width: 320px; height: 100%; padding: .25em; margin: .25em;"
+                class="mb-2"
+            >
+            <b-card-text>
+              {{ movie.overview | truncate(220, '...') }}
+            </b-card-text>
+
+            <router-link :to="{name: 'detail', params: { movie: movie.slug }}">
+              <b-button pill variant="outline-info">O</b-button>
+            </router-link>
+          </b-card>
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Home',
+  filters: {
+    truncate: (text, length, ending) => {
+      return text.substring(0, length) + ending;
+    }
+  }
 }
 </script>
 
 <style scoped>
+    h1 {
+        color: #FFF;
+        padding: 1em;
+    }
+    .movie-row {
+        width: 100%;
+    }
+    .movie-column {
+        padding: 1em;
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import BootstrapVue from 'bootstrap-vue'
 import VueRouter from 'vue-router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
@@ -11,19 +13,28 @@ Vue.config.productionTip = false
 Vue.use(BootstrapVue)
 Vue.use(VueRouter)
 
+
+axios.defaults.headers.common['Authorization'] = 'Bearer Wookie2019'
+Vue.use(VueAxios, axios)
+
 import Home from './components/Home.vue'
 import Detail from './components/Detail.vue'
 
 const routes = [
   { path: '*', component: Home },
-  { path: '/detail/:movie', component: Detail }
+  { path: '/detail/:movie', component: Detail, name: 'detail' }
 ]
 
 const router = new VueRouter({
   routes
 })
 
-new Vue({
-    router,
-    render: h => h(App),
-}).$mount('#app')
+axios.get('https://wookie.codesubmit.io/movies').then((response) => {
+    new Vue({
+        router,
+        data: {
+            movies: response.data.movies
+        },
+        render: h => h(App),
+    }).$mount('#app')
+})
